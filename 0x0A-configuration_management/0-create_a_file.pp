ef6d1e77ce282e2496986824e6d23df65de8e0f4
fikayosalu <salufikayo@gmail.com>
2025-01-20 20:52:21 +0100

    this is okay

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
 # Create a file 'school' with contents 'I love Puppets' in /tmp
 
 file { '/tmp/school':
-  owner   => 'www-data',
+  content => 'I love Puppet',
   mode    => '0744',
-  content => 'I love Puppet'
+  owner   => 'www-data',
+  group   => 'www-data'
 }
