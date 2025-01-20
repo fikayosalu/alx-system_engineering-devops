5918c5459100e9c97ca52a7cd5e0fe266ddaead0
fikayosalu <salufikayo@gmail.com>
2025-01-20 21:36:21 +0100

    this work

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,15 @@
 # Installs flask from pip3
 
+exec { 'add-python3.8-ppa':
+  command => '/usr/bin/add-apt-repository -y ppa:deadsnakes/ppa && /usr/bin/apt-get update',
+  path    => '/usr/bin:/usr/sbin:/bin:/sbin',
+  unless  => 'test -f /etc/apt/sources.list.d/deadsnakes-ubuntu-ppa-focal.list',
+}
+
 package { 'python3.8':
   ensure   => '3.8.10',
-  provider => 'pip3',
+  provider => 'apt',
+  require  => Exec['add-python3.8-ppa'],
 }
 
 package { 'Flask':
