c67a6827691145653ec5508293ae5249088c4d18
fikayosalu <salufikayo@gmail.com>
2025-01-20 21:24:28 +0100

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,17 @@
 # Installs flask from pip3
 
-package { 'flask':
+package { 'python3.8':
+  ensure   => '3.8.10',
+  provider => 'pip3',
+}
+
+package { 'Flask':
   ensure   => '2.1.0',
-  name     => 'flask',
-  provider => 'pip3'
+  provider => 'pip3',
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['Flask'],
 }
